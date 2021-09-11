4a7f96e910fef3f1f24db1ec4920f437fd2e9c2f
Sam Oberg <samueloberg@hotmail.com>
2021-09-11 13:45:12 -0700

    first few tutorials

---
+++ Beginner/variable.py
@@ -0,0 +1,6 @@
+characterName = "George"
+
+print("There once was a man named " + characterName + ", ")
+print("he was 70 years old. ")
+print("He really liked the name " + characterName + ", ")
+print("but did not like being 70.")

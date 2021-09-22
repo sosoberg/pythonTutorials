d6fe4c4c925e070d90899a1b99a89ff006af1a77
Sam Oberg <samueloberg@hotmail.com>
2021-09-21 18:14:44 -0700

    multichoice quiz

---
+++ Beginner/modulesTutorial/multipleChoiceQuiz/app.py
@@ -0,0 +1,23 @@
+from Question import Question
+
+questionPrompts = [
+    "What color are apples? \n(a) Red/Green\n(b) Purple\n(c) Orange\n\n"
+    "What color are bananas? \n(a) Teal\n(b) Magenta\n(c) Yellow\n\n"
+    "What color are strawberries? \n(a) Yellow\n(b) Red\n(c) Blue\n\n"
+]
+
+questions = [
+    Question(questionPrompts[0], "a"),
+    Question(questionPrompts[1], "c"),
+    Question(questionPrompts[2], "b")
+]
+
+def runTest(questions):
+    score = 0
+    for question in questions:
+        answer = input(question.prompt)
+        if answer == question.answer:
+            score += 1
+    print("You got " + str(score) + "/" + str(len(questions)) + " correct")
+
+runTest(questions)
